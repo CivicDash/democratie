f2cde8d8ec72643744ad6c5cbfaad4657635df49
CivicDash Dev <dev@civicdash.fr>
2026-01-06 10:46:06 +0000

    🔧 Fix config mail - DSN avec verify_peer=0 pour Docker interne

--- config/mail.php
+++ config/mail.php
@@ -47,6 +47,8 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            // Désactiver la vérification SSL pour les connexions internes Docker
+            'verify_peer' => env('MAIL_VERIFY_PEER', true),
         ],
 
         'ses' => [
